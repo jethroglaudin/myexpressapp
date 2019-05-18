10a9037edf62dab8861f7fc32f82932897760d35
Jethro Glaudin <jethroglaudin@Jethros-Air.fios-router.home>
2019-05-18 14:56:05 -0400

    updated server for POST requests updated static files also

--- public/script.js
+++ public/script.js
@@ -1,17 +1,36 @@
 const getUserInfo = () => {
-   const input = document.querySelector("#userName").value
-//    console.log(`ES6 ${input}`)
-    const url = '/showprofile/' + input
+  const input = document.querySelector("#userName").value;
+  console.log(input)
 
-   axios.get(url)
-    .then(response => {
-        document.querySelector("#userInfo").innerHTML = response.data
-        
-       
-    })
-   
+  //    console.log(`ES6 ${input}`)
+  const url = "/showprofile/" + input;
+
+  axios.get(url).then(response => {
+    document.querySelector("#userInfo").innerHTML = response.data;
+  });
+};
+
+const handleSubmit = () => {
+  const username = document.querySelector("#user-Name").value;
+  console.log(username)
+  const id = document.querySelector("#userId").value;
+  const message = document.querySelector("#userMsg").value;
+
+  const url2 = "api/";
+
+  axios.post(url2, {  username, id, message }).then(response => {
+    console.log(response);
+    document.querySelector("#submitInfo").innerHTML = response.data;
+  });
+};
+
+const getAllUsers = () => {
+    const url3 = '/getallusers'; 
+    axios.get(url3)
+        .then(response => {
+            document.querySelector('#result').innerHTML = JSON.stringify(response.data)
+        })
 }
-
 
 // function getUserInfo(){
 //    const input = document.getElementById("userName").value
--- server.js
+++ server.js
@@ -1,23 +1,37 @@
 const express = require('express')
+const bodyParser = require('body-parser');
 const app = express()
 const port = process.env.Port || 4000;
 app.use(express.static("public"));
 // same as app.use("/", express.static("public"));
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => res.send('Bonjour, Jethro!'))
+// app.get('/', (req, res) => res.send('Bonjour, Jethro!'))
+const data = [];
 
-app.get('/api', (req, res) => {
-    //reques can send over queries, paramaters, Json, files..
+app.post('/api', (req, res) => {
+    //request can send over queries, paramaters, Json, files..
     //usually you are sending out queries and params
-    const userName = req.query.username;
-    const userId = req.query.id;
-    const message = req.query.message;
+    const userName = req.body.username;
+    const userId = req.body.id;
+    const message = req.body.message;
+    const temp = {
+        username: userName,
+        id: userId,
+        message: message
+    }
+    data.push(temp)
+    console.log(data)
     console.log(userName);
     console.log(userId);
     console.log(message);
     const reply = `My name is ${userName}. My userId is ${userId}, and I wanted to say ${message}.`
 
     res.send(reply);
+})
+
+app.get('/getallusers', (req, res) =>{
+    res.send(data);
 })
 
 app.get('/showprofile/:username', (req, res) => {
