d0624e196daa7ab12b6bfc2fa61bd06bb3e37025
Jethro Glaudin <jethroglaudin@Jethros-Air.fios-router.home>
2019-05-20 21:00:37 -0400

    updated server.js, added models directory, added new Schema

--- server.js
+++ server.js
@@ -1,37 +1,40 @@
 const express = require('express')
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const keys = require('./keys')
+const User = require('./models/User')
 const app = express()
 const port = process.env.Port || 4000;
 app.use(express.static("public"));
 // same as app.use("/", express.static("public"));
 app.use(bodyParser.json());
+//connecting mongoDB or in this case mongoDB Atlas
+mongoose.connect(keys.mongoDBUrl, {useNewUrlParser: true}).then((err) =>  console.log("DB connected"));
 
 // app.get('/', (req, res) => res.send('Bonjour, Jethro!'))
-const data = [];
+
 
 app.post('/api', (req, res) => {
     //request can send over queries, paramaters, Json, files..
     //usually you are sending out queries and params
     const userName = req.body.username;
-    const userId = req.body.id;
     const message = req.body.message;
-    const temp = {
+    const data = {
         username: userName,
-        id: userId,
         message: message
     }
-    data.push(temp)
-    console.log(data)
-    console.log(userName);
-    console.log(userId);
-    console.log(message);
-    const reply = `My name is ${userName}. My userId is ${userId}, and I wanted to say ${message}.`
+    console.log(data);
+    const user = new User(data)
+    user.save().then(() => {
+        console.log(`New user created`)
+        res.send(data);
+    })
 
-    res.send(reply);
+   
 })
 
 app.get('/getallusers', (req, res) =>{
-    res.send(data);
+    res.send("data");
 })
 
 app.get('/showprofile/:username', (req, res) => {
