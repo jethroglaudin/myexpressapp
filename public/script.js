fc57fdbfa238e299ee4a222bde4c64e80b6dff07
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-06-03 23:08:47 -0400

    updated script.js and style.css

--- public/script.js
+++ public/script.js
@@ -1,16 +1,16 @@
+
 const getUserInfo = () => {
   const input = document.querySelector("#userName").value;
-  console.log(input)
+  const url = "/api/showprofile/" + input;
 
-  //    console.log(`ES6 ${input}`)
-  const url = "api/showprofile/" + input;
-
-  axios.get(url).then(response => {
+  axios.get(url)
+  .then(response => {
     const listItems = response.data.map(element => {
       return (
         "<li>"
         + "Name: "
-        + element.username
+        + (element.username ? element.username : " " +
+        element.username + " was not found.")
         + " | "
         + "message: "
         + (element.message ? element.message : " " +
@@ -18,7 +18,7 @@ const getUserInfo = () => {
         + "</li>"
       )
     })
-    document.querySelector("#userInfo").innerHTML = JSON.stringify(response.data);
+    document.querySelector("#userInfo").innerHTML = "<ul>" + listItems.join("\n") + "</ul>"
     console.log(response)
   });
 };
@@ -34,15 +34,49 @@ const handleSubmit = () => {
   const url2 = "api/";
 
   axios.post(url2, {  username, message }).then(response => {
-    console.log(response);
+    // console.log(response);
     document.querySelector("#submitInfo").innerHTML = response.data;
   });
 };
 
 const getAllUsers = () => {
-    const url3 = 'api/getallusers'; 
+    const url3 = '/api/getallusers'; 
     axios.get(url3)
         .then(response => {
-            document.querySelector('#result').innerHTML = JSON.stringify(response.data)
+          const listItems = response.data.map(item => {
+            return (
+              "<tr>"
+              + "<td>"
+              + item.username
+              + "</td>"
+              + "<td>"
+              + (item.message ? item.message : " " +
+              item.username + " did not leave a message")
+              + "</td>"
+              + "</tr>"
+
+              // "<li class='get-all-users'>"
+              // + "<strong>"
+              // + "Name: "
+              // + "</strong>"
+              // + item.username
+              // + " | "
+              // + "<strong>"
+              // + "Message: "
+              // + "</strong>"
+              // +  (item.message ? item.message : " " +
+              // item.username + " did not leave a message.")
+            )
+          })
+            document.querySelector('#result').innerHTML = "<table style='width:100%'>"  +
+            "<tr>"
+            + "<th>"
+            + 'Username'
+            + "</th>"
+            + "<th>"
+            + 'Message'
+            + "</th>"
+            + "</tr>" +  listItems.join(" ") + "</table>"
+            
         })
 }
