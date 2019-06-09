b79fa31c6e62272719dc4000cbbbb390efd9b588
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-06-08 22:03:02 -0400

    updated App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,47 @@
 import React, { Component } from "react";
-import Registration from './components/Registration'
-import UserInfo from './components/UserInfo';
-import GetAllUsers from './components/AllUsers';
+import Registration from "./components/Registration";
+import UserInfo from "./components/UserInfo";
+import AllUsers from "./components/AllUsers";
 class App extends Component {
+  state = {
+    data: "",
+    pageView: ""
+  };
+
+  updateData = (newData) =>{
+    this.setState({
+      data: newData
+    })
+  }
 
   render() {
     return (
       <div className="App">
         <h2>My Express App</h2>
-        <Registration />
+        <a
+          style={{ margin: "10px" }}
+          href="#"
+          onClick={() => this.setState({ pageView: <Registration /> })}
+        >
+          Register
+        </a>
+        <a
+          style={{ margin: "10px" }}
+          href="#"
+          onClick={() => this.setState({ pageView: <UserInfo data={this.state.data}/> })}
+        >
+          User Info
+        </a>
+        <a
+          style={{ margin: "10px" }}
+          href="#"
+          onClick={() => this.setState({ pageView: <AllUsers /> })}
+        >
+          All Users
+        </a>
         <br />
-        <UserInfo />
-        <br />
-        <GetAllUsers />
+
+        {this.state.pageView}
       </div>
     );
   }
